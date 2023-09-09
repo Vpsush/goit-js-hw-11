06844f7a7530d9c611e41fed342ab0b023362f60
Vpsush <vpsush2023@gmail.com>
2023-09-09 22:02:49 +0300

    cbfxg

--- src/api-service.js
+++ src/api-service.js
@@ -1,48 +1,6 @@
 // import axios from 'axios';
 // // import Notify from 'notiflix';
-// import Notiflix from 'notiflix';
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.params = {
-//   key: '39094662-f0479bb8b89274a4b188f6f08',
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: true,
-//   per_page: 40,
-// };
 
-// export default class NewsApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-
-//   async fetchArticles() {
-//     const url = `?&q=${this.searchQuery}&page=${this.page}`;
-
-//     const fetchByUrl = await axios.get(url);
-//     this.page += 1;
-//     console.log(fetchByUrl);
-//     // this.incrementPage();
-
-//     return fetchByUrl;
-//     // } catch (error) {
-//     // Notiflix.Notify.failure(
-//     //   'Sorry, there are no images matching your search query. Please try again.'
-//     // );
-//     // }
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     return (this.searchQuery = newQuery);
-//   }
-// }
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
@@ -68,9 +26,9 @@ export class NewsApiService {
       this.page += 1;
       return fetchByUrl.data.hits;
     } catch (error) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+      // Notiflix.Notify.failure(
+      //   'Sorry, there are no images matching your search query. Please try again.'
+      // );
       throw error;
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import './sass/_common.scss';
+import Notify from 'notiflix';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import { NewsApiService } from './api-service';
@@ -7,21 +8,54 @@ const writeInForm = document.querySelector('#search-form');
 const galleryFill = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const newsApiService = new NewsApiService();
+let isShown = 0;
+
+loadMoreBtn.classList.replace('is-hidden', 'loadMoreBtn');
 
 writeInForm.addEventListener('submit', searchSubmitPictures);
 loadMoreBtn.addEventListener('click', loadMore);
 
 function searchSubmitPictures(e) {
   e.preventDefault();
-  newsApiService.query = e.currentTarget.elements.searchQuery.value;
+  loadMoreBtn.classList.replace('is-hidden', 'loadMoreBtn');
+
+  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
   newsApiService.resetPage();
+
+  if (newsApiService.query === '') {
+    Notiflix.Notify.warning('Please, fill the main field');
+    return;
+  }
+
+  // fetchGallery();
+
   newsApiService
     .fetchArticles()
     .then(pictures => renderList(pictures, galleryFill))
     .catch(error => console.log(error));
+
+  const result = newsApiService.fetchArticles();
+  const { hits, total } = result;
+  isShown += hits.length;
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+
+    if (isShown < hits.length) {
+      Notify.success(`Hooray! We found ${total} images.`);
+    }
+    if (isShown >= hits.length) {
+      Notiflix.Notify.failure(
+        `We're sorry, but you've reached the end of search results.`
+      );
+    }
+  }
+  loadMoreBtn.classList.replace('is-hidden', 'loadMoreBtn');
 }
 
-function loadMore() {
+function loadMore(e) {
+  e.preventDefault();
   newsApiService
     .fetchArticles()
     .then(pictures => renderList(pictures, galleryFill))
@@ -43,8 +77,9 @@ const renderList = (array, container) => {
       } = picture;
       return `
         <div class="photo-card" id="${id}">
-        <a class="gallery__link" href="${largeImageURL}">
-        <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <a class="gallery__link" href="${largeImageURL}" >
+        <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" width = 400 height=300/>
+        </a>
     <div class="info">
       <p class="info-item">
         <b>Likes ${likes}</b>
@@ -64,3 +99,31 @@ const renderList = (array, container) => {
     .join('');
   container.insertAdjacentHTML('beforeend', markup);
 };
+// async function fetchGallery(e) {
+//   e.preventDefault();
+// const result = await newsApiService.fetchGallery();
+// const { hits, total } = result;
+// isShown += hits.length;
+// if (!hits.length) {
+//   Notify.failure(
+//     `Sorry, there are no images matching your search query. Please try again.`
+//   );
+
+//   if (isShown < hits.length) {
+//     Notify.success(`Hooray! We found ${total} images.`);
+//   }
+//   if (isShown >= hits.length) {
+//     Notiflix.Notify.failure(
+//       `We're sorry, but you've reached the end of search results.`
+//     );
+//   }
+// }
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
